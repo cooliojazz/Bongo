8f41ecfb519998c0b3f290fcac525122cc485886
cooliojazz <rickytalbot@hotmail.com>
2022-12-28 18:19:58 -0700

    Add conditions

--- src/main/java/io/github/noeppi_noeppi/mods/bongo/data/WinCondition.java
+++ src/main/java/io/github/noeppi_noeppi/mods/bongo/data/WinCondition.java
@@ -6,11 +6,16 @@ import net.minecraft.world.phys.shapes.BooleanOp;
 import java.util.function.Function;
 
 import static io.github.noeppi_noeppi.mods.bongo.data.WinValues.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum WinCondition {
 
     ONE("bongo.one", one()),
+	TWO("bongo.two", when(sum(count(VALUES_ROWS), count(VALUES_COLS), count(VALUES_DIAGONAL)), 2)),
+    THREE("bongo.three", when(sum(count(VALUES_ROWS), count(VALUES_COLS), count(VALUES_DIAGONAL)), 3)),
     ALL("bongo.all", all()),
+	HALF("bongo.half", half()),
     ROWS("bongo.rows", when(VALUES_ROWS)),
     COLUMNS("bongo.columns", when(VALUES_COLS)),
     DIAGONALS("bongo.diagonals", when(VALUES_DIAGONAL)),
@@ -62,6 +67,28 @@ public enum WinCondition {
             return false;
         };
     }
+	
+    private static Function<TeamCompletion, Boolean> when(Function<TeamCompletion, Integer> c, int count) {
+        return completion -> c.apply(completion) >= count;
+    }
+	
+	private static Function<TeamCompletion, Integer> count(int[][] winValues) {
+        return completion -> {
+            int completions = 0;
+            for (int[] win : winValues) {
+				boolean complete = true;
+                for (int slot : win) {
+                    if (!completion.has(slot)) {
+						complete = false;
+						break;
+					}
+                }
+                if (complete) completions++;
+            }
+            return completions;
+        };
+    }
+
 
     @SuppressWarnings("SameParameterValue")
     private static Function<TeamCompletion, Boolean> compose(BooleanOp combine, WinCondition c1, WinCondition c2) {
@@ -71,6 +98,10 @@ public enum WinCondition {
     @SuppressWarnings("SameParameterValue")
     private static Function<TeamCompletion, Boolean> compose(BooleanOp combine, WinCondition c1, WinCondition c2, WinCondition c3) {
         return completion -> combine.apply(combine.apply(c1.won(completion), c2.won(completion)), c3.won(completion));
+    }
+	
+    private static Function<TeamCompletion, Integer> sum(Function<TeamCompletion, Integer>... cs) {
+        return completion -> Stream.of(cs).collect(Collectors.summingInt(c -> c.apply(completion)));
     }
 
     private static Function<TeamCompletion, Boolean> one() {
@@ -92,4 +123,15 @@ public enum WinCondition {
             return true;
         };
     }
+	
+	private static Function<TeamCompletion, Boolean> half() {
+        return completion -> {
+			int completed = 0;
+            for (int i = 0; i < 25; i++) {
+                if (completion.has(i)) completed++;
+            }
+            return completed >= 13;
+        };
+    }
+
 }
